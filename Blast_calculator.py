971416bbbb0231775d6735a811e2d121accaf2c9
Maulen Yerbolatov <erbolatov.maulen@gmail.com>
2021-04-01 18:47:50 +0600

    Added functions

--- Blast_calculator.py
+++ Blast_calculator.py
@@ -17,18 +17,12 @@ mass = 1 #kg
 volume = mass/(0.08988*29.5)
 E = 3500000*volume #J
 
-def blast_params():
+def overpressure():
     P_file = json.load(
         open(path + "\\overpressure\\{}.json".format(blast_strenght), 'r'))
     P_df = pd.DataFrame(P_file)
     P_arr = P_df.to_numpy()
     P_arr=P_arr.astype(np.float32)
-
-    T_file = json.load(
-        open(path + "\\duration\\{}.json".format(blast_strenght), 'r'))
-    T_df = pd.DataFrame(T_file)
-    T_arr = T_df.to_numpy()
-    T_arr = T_arr.astype(np.float32)
 
     n = 100/((P_a/E)**(1/3))
     distance = np.arange(1, n+1, 1, dtype=float)
@@ -37,14 +31,25 @@ def blast_params():
     k = np.interp(scaled_distance, P_arr[:, 0], P_arr[:, 1])
     #add 0.24
     overpressure = np.multiply(k, 100)
+    return overpressure
+
+def duration():
+    T_file = json.load(
+        open(path + "\\duration\\{}.json".format(blast_strenght), 'r'))
+    T_df = pd.DataFrame(T_file)
+    T_arr = T_df.to_numpy()
+    T_arr = T_arr.astype(np.float32)
 
     l = np.interp(scaled_distance, T_arr[:, 0], T_arr[:,1])
     calc1 = ((E/P_a) ** (1/3)) * 1/a
     positive_phase_duration = np.multiply(calc1, l)
+    return positive_phase_duration
 
+def impulse():
     impulse = np.fromfunction(
         lambda overpressure, positive_phase_duration: 0.5*overpressure*positive_phase_duration, (1, len(distance)+1), dtype=float)
     impulse = np.multiply(0.5*overpressure, positive_phase_duration)
+    return impulse
 
 #print(len(overpressure))
 #print(T_arr)
