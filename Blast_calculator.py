191e0cb65dd805347d2a6436129a39a24bfd20a3
Maulen Yerbolatov <erbolatov.maulen@gmail.com>
2021-04-01 21:50:33 +0600

    Edit of some functions and 'print' tests deleted

--- Blast_calculator.py
+++ Blast_calculator.py
@@ -15,8 +15,7 @@ mass = 1 #kg
 
 def energy(mass):
     volume = mass/(0.08988*29.5)
-    E = 3500000*volume
-    return E
+    return 3500000*volume
 
 def scaled_dist():
     n = 100/((P_a/energy(mass))**(1/3))
@@ -32,7 +31,7 @@ def overpressure():
     P_arr = P_df.to_numpy()
     P_arr=P_arr.astype(np.float32)
 
-    k = np.interp(scaled_dist, P_arr[:, 0], P_arr[:, 1])
+    k = np.interp(scaled_dist(), P_arr[:, 0], P_arr[:, 1])
     #add 0.24
     overpressure = np.multiply(k, 100)
     return overpressure
@@ -44,42 +43,28 @@ def duration():
     T_arr = T_df.to_numpy()
     T_arr = T_arr.astype(np.float32)
 
-    l = np.interp(scaled_dist, T_arr[:, 0], T_arr[:,1])
+    l = np.interp(scaled_dist(), T_arr[:, 0], T_arr[:,1])
     calc1 = ((energy(mass)/P_a) ** (1/3)) * 1/a
     positive_phase_duration = np.multiply(calc1, l)
     return positive_phase_duration
 
 def impulse():
-    impulse = np.fromfunction(
-        lambda overpressure, positive_phase_duration: 0.5*overpressure*positive_phase_duration, (1, len(scaled_dist)+1), dtype=float)
+    sc_dist = scaled_dist()
     impulse = np.multiply(0.5*overpressure(), duration())
     return impulse
 
-#print(len(overpressure))
-#print(T_arr)
-#print(P_arr)
-#print(len(positive_phase_duration))
-#print(len(impulse))
-#print(len(scaled_distance))
-#print(o)
-#print(T_s)
-#print(P_s)
-#print(dist)
-#print(to_add)
-#print(volume)
-#def plotting_blast():
-
-fig, axs = plt.subplots(3, sharex = True, sharey = False)
-axs[0].plot(scaled_dist(), overpressure(), '-')
-axs[1].plot(scaled_dist(), duration(), '-')
-axs[2].plot(scaled_dist(), impulse(), '-')
-axs[0].set_title('side-on overpressure, kPa')
-axs[1].set_title('positive phase duration, s')
-axs[2].set_title('impulse, kPa*s')
-axs[0].grid()
-axs[1].grid()
-axs[2].grid()
-plt.show()
+def plotting():
+    fig, axs = plt.subplots(3, sharex = True, sharey = False)
+    axs[0].plot(scaled_dist(), overpressure(), '-')
+    axs[1].plot(scaled_dist(), duration(), '-')
+    axs[2].plot(scaled_dist(), impulse(), '-')
+    axs[0].set_title('side-on overpressure, kPa')
+    axs[1].set_title('positive phase duration, s')
+    axs[2].set_title('impulse, kPa*s')
+    axs[0].grid()
+    axs[1].grid()
+    axs[2].grid()
+    plt.show()
 
 #def 
 
