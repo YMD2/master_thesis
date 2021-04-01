2f197f12cdfd6e93697b609324c7392cf377a3ce
Maulen Yerbolatov <erbolatov.maulen@gmail.com>
2021-04-01 21:24:26 +0600

    Added functions

--- Blast_calculator.py
+++ Blast_calculator.py
@@ -1,5 +1,3 @@
-#! / usr / bin /env python3
-
 import math
 import numpy as np
 import pandas as pd
@@ -14,8 +12,18 @@ P_a = 101325 #Pa
 path = os.getcwd()
 blast_strenght = 10
 mass = 1 #kg
-volume = mass/(0.08988*29.5)
-E = 3500000*volume #J
+
+def energy(mass):
+    volume = mass/(0.08988*29.5)
+    E = 3500000*volume
+    return E
+
+def scaled_dist():
+    n = 100/((P_a/energy(mass))**(1/3))
+    distance = np.arange(1, n+1, 1, dtype=float)
+    scaled_distance_coeff = ((P_a/energy(mass))**(1/3))
+    scaled_distance = np.multiply(distance, scaled_distance_coeff)
+    return scaled_distance
 
 def overpressure():
     P_file = json.load(
@@ -24,11 +32,7 @@ def overpressure():
     P_arr = P_df.to_numpy()
     P_arr=P_arr.astype(np.float32)
 
-    n = 100/((P_a/E)**(1/3))
-    distance = np.arange(1, n+1, 1, dtype=float)
-    scaled_distance_coeff = ((P_a/E)**(1/3))
-    scaled_distance = np.multiply(distance, scaled_distance_coeff)
-    k = np.interp(scaled_distance, P_arr[:, 0], P_arr[:, 1])
+    k = np.interp(scaled_dist, P_arr[:, 0], P_arr[:, 1])
     #add 0.24
     overpressure = np.multiply(k, 100)
     return overpressure
@@ -40,15 +44,15 @@ def duration():
     T_arr = T_df.to_numpy()
     T_arr = T_arr.astype(np.float32)
 
-    l = np.interp(scaled_distance, T_arr[:, 0], T_arr[:,1])
-    calc1 = ((E/P_a) ** (1/3)) * 1/a
+    l = np.interp(scaled_dist, T_arr[:, 0], T_arr[:,1])
+    calc1 = ((energy(mass)/P_a) ** (1/3)) * 1/a
     positive_phase_duration = np.multiply(calc1, l)
     return positive_phase_duration
 
 def impulse():
     impulse = np.fromfunction(
-        lambda overpressure, positive_phase_duration: 0.5*overpressure*positive_phase_duration, (1, len(distance)+1), dtype=float)
-    impulse = np.multiply(0.5*overpressure, positive_phase_duration)
+        lambda overpressure, positive_phase_duration: 0.5*overpressure*positive_phase_duration, (1, len(scaled_dist)+1), dtype=float)
+    impulse = np.multiply(0.5*overpressure(), duration())
     return impulse
 
 #print(len(overpressure))
@@ -63,21 +67,22 @@ def impulse():
 #print(dist)
 #print(to_add)
 #print(volume)
-def plotting_blast():
-    fig, axs = plt.subplots(3, sharex = True, sharey = False)
-    axs[0].plot(scaled_distance, overpressure, '-')
-    axs[1].plot(scaled_distance, positive_phase_duration, '-')
-    axs[2].plot(scaled_distance, impulse, '-')
-    axs[0].set_title('side-on overpressure, kPa')
-    axs[1].set_title('positive phase duration, s')
-    axs[2].set_title('impulse, kPa*s')
-    axs[0].grid()
-    axs[1].grid()
-    axs[2].grid()
-    plt.show()
+#def plotting_blast():
 
-def 
+fig, axs = plt.subplots(3, sharex = True, sharey = False)
+axs[0].plot(scaled_dist(), overpressure(), '-')
+axs[1].plot(scaled_dist(), duration(), '-')
+axs[2].plot(scaled_dist(), impulse(), '-')
+axs[0].set_title('side-on overpressure, kPa')
+axs[1].set_title('positive phase duration, s')
+axs[2].set_title('impulse, kPa*s')
+axs[0].grid()
+axs[1].grid()
+axs[2].grid()
+plt.show()
 
-parser = argparse.ArgumentParser(description='Process initial values.')
-parser.add_argument('Mass', )
+#def 
+
+#parser = argparse.ArgumentParser(description='Process initial values.')
+#parser.add_argument('Mass', )
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# master_thesis
-Title: The hazard of fragments in accidental H2-air explosions.
